617102f9139a91c080af2a4487dea38e4aced608
Salamzade Islam Emil <Salam_ar93@itstep.edu.az>
2024-01-22 21:44:26 +0400

    todowebservice

---
+++ TodoWebService/Controllers/TodoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoWebService.Data;
+using TodoWebService.Models.DTOs;
+using TodoWebService.Models.DTOs.Todo;
+using TodoWebService.Services.Todo;
+
+namespace TodoWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        [HttpGet("get/{id}")]
+        public async Task<ActionResult<TodoItemDto>> Get(int id)
+        {
+            var item = await _todoService.GetTodoItem(id);
+
+
+            return item is not null
+                ? item
+                : NotFound();
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            var result = await _todoService.DeleteTodo(id);
+
+            return result ? Ok() : BadRequest();
+        }
+
+        [HttpPost("createTodo")]
+
+        public async Task<ActionResult<TodoItemDto>> CreateTodo(CreateTodoItemRequest createTodoItemRequest)
+        {
+            var item = await _todoService.CreateTodo(createTodoItemRequest);
+
+            return item is not null ? item : BadRequest();
+        }
+
+        [HttpPatch("changeStatus/{id}")]
+
+        public async Task<ActionResult<TodoItemDto>> ChangeStatus(int id,bool isCompleted)
+        {
+            var item = await _todoService.ChangeTodoItemStatus(id,isCompleted);
+
+            return item is not null ? item : BadRequest();
+        } 
+    }
+}
---
+++ TodoWebService/Services/Todo/TodoService.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoWebService.Data;
+using TodoWebService.Models.DTOs;
+using TodoWebService.Models.DTOs.Todo;
+using TodoWebService.Models.Entities;
+
+namespace TodoWebService.Services.Todo
+{
+    public class TodoService : ITodoService
+    {
+        private readonly TodoDbContext _context;
+
+        public TodoService(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItemDto?> ChangeTodoItemStatus(int id, bool isCompleted)
+        {
+            try
+            {
+                var todoItem = await _context.TodoItems.FirstOrDefaultAsync(x => x.Id == id);
+                if (todoItem is not null)
+                {
+                    todoItem!.IsCompleted = isCompleted;
+                    _context.TodoItems.Update(todoItem);
+                    await _context.SaveChangesAsync();
+                    return new TodoItemDto(todoItem.Id, todoItem.Text, todoItem.IsCompleted, todoItem.CreatedTime);
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<TodoItemDto?> CreateTodo(CreateTodoItemRequest request)
+        {
+            try
+            {
+                var newTodoItem = new TodoItem()
+                {
+                    Text = request.Text,
+                    CreatedTime = DateTime.Now,
+                    UpdatedTime = DateTime.Now,
+                    IsCompleted = false,
+                };
+                await _context.TodoItems.AddAsync(newTodoItem);
+                await _context.SaveChangesAsync();
+                return new TodoItemDto(newTodoItem.Id, newTodoItem.Text, newTodoItem.IsCompleted, newTodoItem.CreatedTime);
+            }
+            catch (Exception)
+            {
+                //log
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteTodo(int id)
+        {
+            try
+            {
+                var todoItem = _context.TodoItems.FirstOrDefault(x => x.Id == id);
+                if (todoItem is not null)
+                {
+                    _context.TodoItems.Remove(todoItem!);
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                //log;
+                return false;
+            }
+            return false;
+        }
+
+        public Task<List<TodoItemDto>> GetAll(int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TodoItemDto?> GetTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+
+            return todoItem is not null
+                ? new TodoItemDto(
+                    Id: todoItem.Id,
+                    Text: todoItem.Text,
+                    IsCompleted: todoItem.IsCompleted,
+                    CreatedTime: todoItem.CreatedTime)
+                : null;
+        }
+    }
+}
